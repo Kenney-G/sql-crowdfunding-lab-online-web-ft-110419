efd79519accf280031b6823a70180913b544a9b2
Ken Goad <goadke@g.cofc.edu>
2019-12-31 23:18:58 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -33,6 +33,9 @@ end
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   "SELECT users.name, 
   SUM(pledges.amount)
+  FROM projects
+  INNER JOIN pledges ON
+  pledges.project_id = projects.id GROUP BY project.title
   "
 end
 
